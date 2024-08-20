ae52e9ad21d11f8da852f3ef0af159766c463e46
Faizan Raza <faizanraza2308@gmail.com>
2024-08-20 12:35:43 +0530

    Time: 8 ms (66.45%) | Memory: 10.9 MB (20.30%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,10 +11,12 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast= head;
-        while(fast!=nullptr && fast->next!=nullptr){
+        while(fast!=nullptr && fast->next!=nullptr){ ///whatever odd or even no of node it will stops
             slow=slow->next;
             fast=fast->next->next;
-            if(fast==slow) return true;
+            if(fast==slow){    // this is ensure that the is exist bcz if slow=1km/h and fast=2km/
+                return true;                //net velocity is 1km if d is dis then d%1=0(definetly)
+            }
         }
         return false;
        
