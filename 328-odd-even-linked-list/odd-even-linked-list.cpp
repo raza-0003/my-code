c19a3df1adf17207e774aeef2acdead8f7898560
Faizan Raza <faizanraza2308@gmail.com>
2024-09-04 17:46:03 +0530

    Time: 6 ms (80.76%) | Memory: 15.4 MB (53.34%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,41 +11,20 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-    if (head == nullptr || head->next == nullptr) {
+        if(!head) return nullptr;
+        // firstly for odd index-numbers
+        ListNode* odd= head;
+        ListNode* even=head->next;
+        ListNode* evenHead=even;
+        while(even!=nullptr && even->next!=nullptr){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+
+        }
+        odd->next=evenHead;
         return head;
-    }
-    
-    std::vector<int> arr;
-    ListNode* temp = head;
-
-    // Collecting odd-indexed nodes (1st, 3rd, 5th, etc.)
-    while (temp != nullptr) {
-        arr.push_back(temp->val);
-        temp = (temp->next != nullptr) ? temp->next->next : nullptr;
-    }
-    
-    temp = head->next;
-
-    // Collecting even-indexed nodes (2nd, 4th, 6th, etc.)
-    while (temp != nullptr) {
-        arr.push_back(temp->val);
-        temp = (temp->next != nullptr) ? temp->next->next : nullptr;
-    }
-    
-    temp = head;
-    int i = 0;
-    
-    // Reassigning values to the linked list
-    while (temp != nullptr) {
-        temp->val = arr[i];
-        temp = temp->next;
-        i++;
-    }
-    
-    return head;
-}
-
-   
         
-    
+    }
 };
