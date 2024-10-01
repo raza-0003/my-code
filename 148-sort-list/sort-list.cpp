f2ed98cab009b717a87111aa318ae4047fc0f1ab
Faizan Raza <faizanraza2308@gmail.com>
2024-10-01 13:10:09 +0530

    Time: 342 ms (5.10%) | Memory: 191.8 MB (5.29%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,68 +10,55 @@
  */
 class Solution {
 public:
-    void merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
-
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
+    void merge(vector<int>&arr,int low,int mid,int high){
+        vector<int> temp;
+        int left=low; // starting index of left half of arr
+        int right = mid+1; // starting index of right half of arr
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
             temp.push_back(arr[left]);
             left++;
         }
-        else {
+        while(right<=high){
             temp.push_back(arr[right]);
             right++;
         }
+        for(int i=low;i<=high;i++){
+            arr[i]=temp[i-low];
+        }
     }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
+    void mergeSort(vector<int> &arr,int low,int high){
+        if(low>=high) return;
+        int mid = (low + high)/2;
+        mergeSort(arr,low,mid); // left half
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
     }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
-}
-
-    void mergeSort(vector<int> &arr, int low, int high) {
-        if (low >= high) return;
-        int mid = (low + high) / 2 ;
-        mergeSort(arr, low, mid);  // left half
-        mergeSort(arr, mid + 1, high); // right half
-        merge(arr, low, mid, high);  // merging sorted halves
-    }
-    
     ListNode* sortList(ListNode* head) {
+        ListNode* temp = head;
         vector<int> arr;
-        ListNode* temp=head;
-        int cnt=0;
-        while(temp){
-            cnt++;
+        while(temp!=nullptr){
             arr.push_back(temp->val);
             temp=temp->next;
         }
-        mergeSort(arr,0,cnt-1);
-        temp=head;
+        int len = arr.size();
+        mergeSort(arr,0,len-1);
+        temp = head;
         int i=0;
-        while(temp){
-            temp->val=arr[i];
-            i++;
+        while(temp!=nullptr){
+            temp->val = arr[i];
+            i=i+1;
             temp=temp->next;
         }
-        return head; 
+        return head;    
     }
 };
