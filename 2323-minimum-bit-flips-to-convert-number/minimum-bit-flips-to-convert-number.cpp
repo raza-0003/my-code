9ad9c1893d9b326023fc254cbd44defc1ee5b9e9
Faizan Raza <faizanraza2308@gmail.com>
2024-10-06 16:35:05 +0530

    Time: 3 ms (40.69%) | Memory: 7.5 MB (92.73%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -3,6 +3,7 @@ public:
     int minBitFlips(int start, int goal) {
         int cnt = 0;
         for(int i=0;i<32;i++){
+            //basically we have to apply condition when the bits are diff so that time inc count by 1 
             if(((start) & (1<<i)) != ((goal) & (1<<i))){
                 cnt++;
             }
