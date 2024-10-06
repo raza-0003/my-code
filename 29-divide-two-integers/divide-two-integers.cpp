fd2a7766f7498477565b8c071e659b3940b76baa
Faizan Raza <faizanraza2308@gmail.com>
2024-10-06 12:53:58 +0530

    Time: 5 ms (28.39%) | Memory: 8.5 MB (34.35%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend==divisor) return 1;
+        long n = abs(dividend);
+        long d = abs(divisor);
+        bool sign = true;
+        if(dividend>=0 && divisor<0) sign = false;
+        if(dividend<=0 && divisor>0) sign = false;
+        
+        long res = 0;
+        // check condition if no is negative or divisor is negative
+        while(n>=d){
+            int cnt = 0;
+            while(d << (cnt+1) <= n){
+                cnt++;
+            }
+            res+= 1<<(cnt);
+            n = n - (d<<(cnt));
+        }
+        if((res == 1<<31) && (sign == true)){
+            return INT_MAX;
+        }
+        if((res == 1<<31) && (sign == false)){
+            return INT_MIN;
+        }
+
+        return sign ? res : -res;
+        
+    }
+};
