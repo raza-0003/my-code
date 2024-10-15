beb2ea7a627be4bd4e3ea166332c149b5d8f2b21
Faizan Raza <faizanraza2308@gmail.com>
2024-10-16 00:36:10 +0530

    Time: 138 ms (5.15%) | Memory: 26.2 MB (96.91%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> ans(nums.size(),-1);
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size()+i+1;j++){
+            for(int j=i+1;j<nums.size()+i;j++){
                 int ind = j%nums.size();
                 if(nums[ind]>nums[i]){
                     ans[i]=nums[ind];
