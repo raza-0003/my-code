bc53f27ee48f24b4d4c26b67769b888092496e03
Faizan Raza <faizanraza2308@gmail.com>
2024-10-19 11:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (17.10%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -16,11 +16,27 @@ public:
         if(root == nullptr){
             return ans;
         }
-        vector<int>left = postorderTraversal(root->left);
-        ans.insert(ans.end(),left.begin(),left.end());
-        vector<int>right = postorderTraversal(root->right);
-        ans.insert(ans.end(),right.begin(),right.end());
-        ans.push_back(root->val);
-        return ans;    
+        stack<TreeNode*>st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            // this st1 will contian root right left and st2 also
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=nullptr){
+                st1.push(root->left);
+            }
+            if(root->right!=nullptr){
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty()){
+            // to revese root right left (to get left right root)(to reverse) have push these into ans list
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+
+        
     }
 };
