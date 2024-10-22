24472a8492a1a86bff58eadb95f296ae2c41c24b
Faizan Raza <faizanraza2308@gmail.com>
2024-10-22 23:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.88%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,9 +12,11 @@
 class Solution {
 public:
     void recursion_right(TreeNode* root,int level,vector<int>&ans){
+        //it is traversal like root right left;
         if(root==nullptr) return;
         if(level == ans.size()){
-            ans.push_back(root->val);
+            // above conditio ensure that only one element(right side->view) will store into ans 
+            ans.push_back(root->val); 
         }
         recursion_right(root->right,level+1,ans);
         recursion_right(root->left,level+1,ans);
