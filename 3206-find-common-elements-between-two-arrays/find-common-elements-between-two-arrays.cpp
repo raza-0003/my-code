e70ef94b1afb28f4c06b3fb809576ab22a913465
Faizan Raza <faizanraza2308@gmail.com>
2024-12-21 12:27:09 +0530

    Time: 21 ms (20.71%) | Memory: 69.5 MB (51.20%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>mpp1,mpp2;
-        vector<int>ans(2,0);
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        unordered_map<int,int> mpp1, mpp2;
+        vector<int> ans(2,0);
         
-        // Count frequencies in nums1
-        for(int i=0;i<n1;i++){
-            mpp1[nums1[i]]++;
+        // Mark presence
+        for(int num : nums1) mpp1[num] = 1;
+        for(int num : nums2) mpp2[num] = 1;
+        
+        // Count all occurrences
+        for(int num : nums1) {
+            if(mpp2.count(num)) ans[0]++;
         }
         
-        // Count frequencies in nums2
-        for(int i=0;i<n2;i++){
-            mpp2[nums2[i]]++;
-        }
-        
-        // Calculate answer1 by checking nums1 elements in nums2
-        for(int i=0;i<n1;i++){
-            if(mpp2.count(nums1[i])){
-                ans[0]++;
-            }
-        }
-        
-        // Calculate answer2 by checking nums2 elements in nums1
-        for(int i=0;i<n2;i++){
-            if(mpp1.count(nums2[i])){
-                ans[1]++;
-            }
+        for(int num : nums2) {
+            if(mpp1.count(num)) ans[1]++;
         }
         
         return ans;
