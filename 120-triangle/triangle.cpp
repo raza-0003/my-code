6c2c76c2ba364f5d146d6819e163ca66ac96398d
Faizan Raza <faizanraza2308@gmail.com>
2024-12-22 17:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (27.52%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&triangle,vector<vector<int>>&dp,int n){
-        if(i == n-1) return triangle[n-1][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = triangle[i][j] + f(i+1,j,triangle,dp,n);
-        int diagnal = triangle[i][j] + f(i+1,j+1,triangle,dp,n);
-        return dp[i][j] = min(down,diagnal);
-    }
     int minimumTotal(vector<vector<int>>&triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>frontRow(n,0);
         for(int j=0;j<n;j++){
-            dp[n-1][j] = triangle[n-1][j];
+            frontRow[j] = triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
+            vector<int>curr(n,0);
             for(int j=i;j>=0;j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagnal = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(down,diagnal);
+                int down = triangle[i][j] + frontRow[j];
+                int diagnal = triangle[i][j] + frontRow[j+1];
+                curr[j] = min(down,diagnal);
             }
+            frontRow = curr;
         }
-        return dp[0][0];
+        return frontRow[0];
         
     }
 };
