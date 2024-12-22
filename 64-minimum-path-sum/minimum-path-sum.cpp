cce4fd630a234d2bcdc71ca55ca98bdfb7ff3e2f
Faizan Raza <faizanraza2308@gmail.com>
2024-12-22 14:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (16.23%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -12,6 +12,23 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,grid,dp);
+        //initialize dp array
+        dp[0][0] = grid[0][0];
+        // filling first row to calculate 
+        for(int i = 1;i<n;i++){
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+        //fillig first coloumn
+        for(int i=1;i<m;i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                    int up = dp[i-1][j];
+                    int left = dp[i][j-1];
+                    dp[i][j] = min(up,left) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
