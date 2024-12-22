652b442617f6d854a364daffa80428279061dffc
Faizan Raza <faizanraza2308@gmail.com>
2024-12-22 17:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (19.87%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -9,8 +9,18 @@ public:
     }
     int minimumTotal(vector<vector<int>>&triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(0,0,triangle,dp,n);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagnal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,diagnal);
+            }
+        }
+        return dp[0][0];
         
     }
 };
