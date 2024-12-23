49408daa641845aa3e942050fd5ef237775eae69
Faizan Raza <faizanraza2308@gmail.com>
2024-12-23 11:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (32.61%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int>factorList;
-        if((n==1 && k==1) || (n==2 && k==2 )) return k; 
-        for(int i=1;i<=n;i++){
-            if(n%i == 0){
-                factorList.push_back(i);
+        // Iterate up to sqrt(n) to collect smaller factors
+        for (int i = 1; i <= n; ++i) {
+            if (n % i == 0) { // Check if i is a factor
+                --k;         // Decrement k
+                if (k == 0) return i; // Return the k-th factor
             }
         }
-        int kthFsize = factorList.size();
-        if(n > kthFsize && k-1 < kthFsize ) return factorList[k-1];
-        else return -1;
-        
+        return -1; // If k factors are not found
     }
 };
+
