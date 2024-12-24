46ac85bb6ed7d23eef5bff249dd349f6c172d4b2
Faizan Raza <faizanraza2308@gmail.com>
2024-12-25 00:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (25.90%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        // If lengths are different, rotation is impossible
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        // Concatenate s with itself
+        string doubled = s + s;
+        
+        // Check if goal is a substring of doubled
+        return doubled.find(goal) != string::npos;
+    }
+};
