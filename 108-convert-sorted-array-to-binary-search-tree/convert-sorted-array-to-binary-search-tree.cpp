e6aa525bb8676b9985fa5f014467048c7c3f3910
Faizan Raza <faizanraza2308@gmail.com>
2024-12-25 18:41:17 +0530

    Time: 117 ms (5.16%) | Memory: 345.7 MB (5.25%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -20,6 +20,7 @@ public:
         return root;
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if(nums.size()==0) return nullptr;
         return buildBst(nums,0,nums.size()-1);
     }
 };
