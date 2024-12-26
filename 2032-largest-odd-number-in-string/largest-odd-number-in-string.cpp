1b6c5b932f61dcd2dc1b15f90672fe0e56e24b54
Faizan Raza <faizanraza2308@gmail.com>
2024-12-26 13:13:52 +0530

    Time: 4 ms (24.01%) | Memory: 17.8 MB (23.42%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         int n = num.size();
-        for(int i=n-1;i>=0;i--){
-            if((num[i] -'0') & 1){
-                return num;
-            }
-            else{
-                num.erase(i,1);
+        for (int i = n - 1; i >= 0; i--) {
+            if ((num[i] - '0') & 1) {  // Check if the digit is odd
+                return num.substr(0, i + 1);  // Return the substring up to this index
+            } else {
+                num.erase(i, 1);  // Remove the even digit
             }
         }
-        return num;
-        
+        return "";  // If no odd digit is found, return an empty string
     }
 };
