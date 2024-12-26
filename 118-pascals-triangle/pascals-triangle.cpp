f7e5e9ec494df01103460fc851efafd88486d847
Faizan Raza <faizanraza2308@gmail.com>
2024-12-26 20:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (32.61%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>pascals(numRows);
+        for(int i=0;i<numRows;i++){
+            pascals[i].resize(i+1,1);
+            for(int j=1;j<i;j++){
+                pascals[i][j] = pascals[i-1][j-1] + pascals[i-1][j];
+            }
+        }
+        return pascals;
+    }
+};
