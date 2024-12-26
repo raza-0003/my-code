900e35be850e3533c3b7f3f6e09282d508efe8ee
Faizan Raza <faizanraza2308@gmail.com>
2024-12-26 18:43:50 +0530

    Time: 150 ms (49.81%) | Memory: 18.3 MB (59.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    bool f(int ind,int target,vector<int>& nums,vector<vector<int>>&dp){
-        if(target == 0) return true;
-        if(ind == 0) return (nums[ind] == target);
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        bool notTake = f(ind-1,target,nums,dp);
-        bool Take = false;
-        if(target >= nums[ind]){
-            Take = f(ind-1,target-nums[ind],nums,dp);
-        }
-        return dp[ind][target] = Take || notTake;
-    }
     bool canPartition(vector<int>& nums) {
         int arrSum = 0;
         int n = nums.size();
@@ -18,8 +7,20 @@ public:
             arrSum += nums[i];
         }
         int k = arrSum/2;
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        vector<vector<bool>>dp(n+1,vector<bool>(k+1,0));
         if(arrSum & 1) return false;
-        return f(n-1,k,nums,dp);
+        // lets try out the tabulation method
+        for(int i=0;i<n;i++) dp[i][0] = true;
+        for(int i=1;i<n;i++){
+            for(int target =0;target<=k;target++){
+                bool notTake = dp[i-1][target];
+                bool Take = false;
+                if(target >= nums[i]){
+                    Take = dp[i-1][target-nums[i]];
+                }
+                dp[i][target] = Take || notTake;
+            }
+        } 
+        return dp[n-1][k];
     }
 };
