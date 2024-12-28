5785a8b2df3e58b74132839fced5a73356bfa9ef
Faizan Raza <faizanraza2308@gmail.com>
2024-12-28 09:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (36.67%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int ptr1 = 0;
-        int ptr2 = 0;
-        int ind = 0;
-        vector<int>ans(n+m,0);
-        while(ptr1<m && ptr2<n){
-            if(nums1[ptr1] < nums2[ptr2]){
-                ans[ind]=nums1[ptr1];
-                ind++;
-                ptr1++;
+        int p1 = m-1;
+        int p2 = n-1;
+        int ind = m+n-1;
+        // merging from the back of array
+        while(p1>=0 && p2>=0){
+            if(nums1[p1]>nums2[p2]){
+                nums1[ind] = nums1[p1];
+                p1--;
             }
             else{
-                ans[ind] = nums2[ptr2];
-                ptr2++;
-                ind++;
+                nums1[ind] = nums2[p2];
+                p2--;
             }
+            ind--;
         }
-        while(ptr1<m){
-            ans[ind]=nums1[ptr1];
-            ptr1++;
-            ind++;
+        while(p2>=0){
+            nums1[ind] = nums2[p2];
+            ind--;
+            p2--;
         }
-        while(ptr2<n){
-            ans[ind]=nums2[ptr2];
-            ptr2++;
-            ind++;
-        }
-        nums1 = ans;
+        
     }
 };
