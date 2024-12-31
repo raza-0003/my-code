9dc83f3c0ee8700a079d83a6312ab539b9ed8e26
Faizan Raza <faizanraza2308@gmail.com>
2024-12-31 10:36:08 +0530

    Time: 10 ms (5.19%) | Memory: 81 MB (5.14%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int globalMax;
-    int f(int index,vector<int>& nums){
+    int f(int index,vector<int>& nums,vector<int>&dp){
         if(index == 0){
             return nums[0];
         }
         //calculating previous sum;
-        int prev = f(index-1,nums);
+        if(dp[index]!=-1) return dp[index];
         // we have choices: 1)go with prev subarray 2)getting alone
-        int withprev = prev + nums[index];
+        int withprev = f(index-1,nums,dp) + nums[index];
         int alone = nums[index];
         int maxEnding = max(withprev,alone);
         globalMax = max(globalMax,maxEnding);
-        return maxEnding;
+        return dp[index] = maxEnding; // note this line is very important(returning maxEnding not globalMax)
     }
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
         globalMax = nums[0];
-        f(nums.size()-1,nums);
+        f(nums.size()-1,nums,dp);
         return globalMax;
         
     }
