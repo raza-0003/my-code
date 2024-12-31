3972e7328776e26f13cf252bacbeb472471e74d6
Faizan Raza <faizanraza2308@gmail.com>
2024-12-31 20:46:04 +0530

    Time: 9 ms (23.02%) | Memory: 25.4 MB (34.39%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         // define the hash set for row,col,box(3*3)
-        unordered_map<int,unordered_set<char>>row;
-        unordered_map<int,unordered_set<char>>col;
-        unordered_map<int,unordered_set<char>>box;
+        vector<unordered_set<char>> row(9), col(9), box(9);
         // lets traversing through the board
         for(int i=0;i<9;i++){  // i-->row  
             for(int j=0;j<9;j++){   // j-->col
