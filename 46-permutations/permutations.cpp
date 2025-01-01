b2da501943055b16ef061080c23547272dc0c709
Faizan Raza <faizanraza2308@gmail.com>
2025-01-01 14:33:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (35.97%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    void perm(int index,vector<int>&arr,vector<vector<int>>&ans){
-        int n = arr.size();
-        if(index == n){
-            ans.push_back(arr);
+    void perm(vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans,vector<int>&freq){
+        if(ds.size() == nums.size()){
+            ans.push_back(ds);
             return;
         }
-        for(int i=index;i<n;i++){
-            swap(arr[index],arr[i]);
-            perm(index+1,arr,ans);
-            swap(arr[index],arr[i]);
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                // it means not visited
+                freq[i] = 1;
+                ds.push_back(nums[i]);
+                perm(ds,nums,ans,freq);
+                freq[i]=0;
+                ds.pop_back();  // pop out the last element from the ds
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
+        vector<int>ds;
         vector<vector<int>>ans;
-        perm(0,nums,ans);
+        vector<int>freq(n,0);
+        perm(ds,nums,ans,freq);
         return ans;
     }
 };
