af750cf3b7124e4a367d9e8d92f5a607626f5f2d
Faizan Raza <faizanraza2308@gmail.com>
2025-01-03 09:57:37 +0530

    Time: 4 ms (83.55%) | Memory: 15.3 MB (46.95%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    int f(int index,int target,vector<int>&nums,vector<vector<int>>&dp){
-        if(index==0){
-            if(target == 0 && nums[0] == 0) return 2;
-            if(target ==0 || target == nums[0]) return 1;
-            return 0;
-        }
-        if(dp[index][target]!=-1) return dp[index][target];
-        int notTake = f(index-1,target,nums,dp);
-        int take = 0;
-        if(nums[index]<=target){
-            take = f(index-1,target-nums[index],nums,dp);
-        }
-        return dp[index][target] = (take + notTake);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int totSum = 0;
@@ -22,7 +8,26 @@ public:
         }
         int aim = (totSum-target)/2;
         if(totSum-target < 0 || (totSum-target)%2 == 1) return 0;
-        vector<vector<int>>dp(n,vector<int>(aim+1,-1));
-        return f(n-1,aim,nums,dp);
+        vector<vector<int>>dp(n,vector<int>(aim+1,0));
+        if(nums[0]==0){
+            dp[0][0]=2; // two cases pick or not pick
+        }
+        else{
+            dp[0][0] = 1;   //only one case not pick
+        }
+        if(nums[0]!=0 && nums[0]<=aim){
+            dp[0][nums[0]] = 1;     //only one case pick
+        }
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=aim;tar++){
+                int notTake = dp[i-1][tar];
+                int take = 0;
+                if(tar>=nums[i]){
+                    take = dp[i-1][tar-nums[i]];
+                }
+                dp[i][tar] = take + notTake;
+            }
+        }
+        return dp[n-1][aim];
     }
 };
