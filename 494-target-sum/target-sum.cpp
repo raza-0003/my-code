6bfa6af73eb9a49f76937a86201c4ca175ba87dc
Faizan Raza <faizanraza2308@gmail.com>
2025-01-03 10:00:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (51.78%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -8,26 +8,27 @@ public:
         }
         int aim = (totSum-target)/2;
         if(totSum-target < 0 || (totSum-target)%2 == 1) return 0;
-        vector<vector<int>>dp(n,vector<int>(aim+1,0));
+        vector<int>prev(aim+1,0),curr(aim+1,0);
         if(nums[0]==0){
-            dp[0][0]=2; // two cases pick or not pick
+            prev[0]=2; // two cases pick or not pick
         }
         else{
-            dp[0][0] = 1;   //only one case not pick
+            prev[0] = 1;   //only one case not pick
         }
         if(nums[0]!=0 && nums[0]<=aim){
-            dp[0][nums[0]] = 1;     //only one case pick
+            prev[nums[0]] = 1;     //only one case pick
         }
         for(int i=1;i<n;i++){
             for(int tar=0;tar<=aim;tar++){
-                int notTake = dp[i-1][tar];
+                int notTake = prev[tar];
                 int take = 0;
                 if(tar>=nums[i]){
-                    take = dp[i-1][tar-nums[i]];
+                    take = prev[tar-nums[i]];
                 }
-                dp[i][tar] = take + notTake;
+                curr[tar] = take + notTake;
             }
+            prev = curr;
         }
-        return dp[n-1][aim];
+        return prev[aim];
     }
 };
