3be4805e04bcafbe158139d2395eeef2b31155e6
Faizan Raza <faizanraza2308@gmail.com>
2025-05-24 16:14:45 +0530

    Time: 3 ms (43.15%) | Memory: 9.4 MB (28.97%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -7,6 +7,7 @@ public:
         for(int i=0;i<s.size();i++){
             char c1 = s[i];
             char c2 = t[i];
+            // mpp1.count(c1) ==> checking that c1 is mapped already in mpp1 or not
             if((mpp1.count(c1) && mpp1[c1] != c2 ) || (mpp2.count(c2) && mpp2[c2] != c1)){
                 return false;
             }
