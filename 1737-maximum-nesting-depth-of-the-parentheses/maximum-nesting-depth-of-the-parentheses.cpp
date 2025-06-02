41519348045ac78c90f8f2de19d9c8e1377b5ac1
Faizan Raza <faizanraza2308@gmail.com>
2025-06-02 23:35:31 +0530

    Time: 1 ms (5.39%) | Memory: 8.4 MB (26.26%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int maxNestDepth = 0;
+        int temp = 0;
+        for(auto ch:s){
+            if(ch == '('){
+                temp++;
+                maxNestDepth=max(maxNestDepth,temp);
+            }
+            else if(ch == ')'){
+                temp--;
+            }
+            else continue;
+        }
+        return maxNestDepth;
+        
+        
+    }
+};
