f55420db7590373e6e1b620318d55e46a4c70c4d
Faizan Raza <faizanraza2308@gmail.com>
2025-06-19 21:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (64.39%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    bool f(int index,vector<int>&nums,vector<int>&dp){
-        int n = nums.size();
-        //covering all the base cases
-        if(index == n-1) return true;
-        if(index>=n) return false;
-        //exploring all the paths 
-        if(dp[index]!=-1) return dp[index];
-        int maxJumps = nums[index];
-        for(int jump =1;jump<=maxJumps;jump++){
-            if(f(index + jump,nums,dp) == true){
-                return dp[index] = true;
-            }
-        }
-        return dp[index] = false;
-    }
     bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1); 
-        return f(0,nums,dp);
-        //start recursion from back side
-        //find(ind,nums) --> signifies that can i reach at (n-1) from index = 0;
+        int n = nums.size();
+        if(nums[0] == 0 && n > 1) return false;
+        if(n == 1) return true;
+        int maxInd = 0;
+        for(int i=0;i<n;i++){
+            if(i > maxInd) return false;
+            maxInd = max(maxInd,nums[i] + i);
+            if(maxInd >= n-1) return true;
+        }
+        return true;
+        
     }
 };
