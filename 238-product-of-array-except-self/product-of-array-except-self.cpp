13b0c3bb9f4e5231338ee93b91fbe94a9fc01f80
Faizan Raza <faizanraza2308@gmail.com>
2025-06-24 13:51:29 +0530

    Time: 7 ms (6.14%) | Memory: 42.2 MB (16.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,33 +2,19 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n,0);
-        long long mul = 1;
-        int cntzero = 0;
+        vector<int>premul(n);
+        vector<int>sufmul(n);
+        premul[0] = 1;
+        sufmul[n-1] = 1;
+        for(int i=1;i<n;i++){
+            premul[i] = nums[i-1] * premul[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            sufmul[i] = nums[i+1] * sufmul[i+1];
+        }
+        vector<int>ans(n);
         for(int i=0;i<n;i++){
-            if(nums[i] == 0){
-                cntzero++;
-            }
-            else{
-                mul *= nums[i];
-            }
-        }
-        // case-1 
-        if(cntzero > 1){
-            return ans;
-        }
-        // case-2
-        if(cntzero == 1){
-            for(int i=0;i<n;i++){
-                if(nums[i] == 0){
-                    ans[i] = mul;
-                    break;
-                }
-            }
-            return ans;
-        }
-        for(int i=0;i<n;i++){
-            ans[i] = (mul / nums[i]);
+            ans[i] = premul[i] * sufmul[i];
         }
         return ans;
         
