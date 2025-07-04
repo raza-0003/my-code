e6d16bc80594548c7afca0208a27413208f181aa
Faizan Raza <faizanraza2308@gmail.com>
2025-07-04 11:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (57.57%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
+        int cand1=0,cand2=0;
+        int cnt1=0,cnt2=0;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-        }
         vector<int>ans;
-        for(auto [num,freq]:mpp){
-            if(freq > n /3){
-                ans.push_back(num);
+        for(int i=0;i<n;i++){
+            if(cnt1 == 0 && nums[i] != cand2){
+                cnt1 = 1;
+                cand1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i] != cand1){
+                cnt2 = 1;
+                cand2 = nums[i];
+            }
+            else if(nums[i] == cand1) cnt1++;
+            else if(nums[i] == cand2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        // since we are not assurre that ki majority element exist or not
+        // upar jo cnt1,cnt2 that does not signify anything brother
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == cand1){
+                cnt1++;
+            }
+            else if(nums[i] == cand2){
+                cnt2++;
+            }
+        }
+        if(cnt1 > n/3){
+            ans.push_back(cand1);
+        }
+        if(cnt2 > n/3){
+            ans.push_back(cand2);
+        }
         return ans;
-        
     }
 };
