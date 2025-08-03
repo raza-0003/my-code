0f9e4ab653f08d5e7b2a199d1c6d7632f96fcbc0
Faizan Raza <faizanraza2308@gmail.com>
2025-08-03 11:42:57 +0530

    Time: 7 ms (4.55%) | Memory: 15.2 MB (25.55%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int find(int i,vector<int>&parent){
+        if(i == parent[i]){
+            return i;
+        }
+        return parent[i] = find(parent[i],parent);
+    }
+    void Union(int x,int y,vector<int>&parent){
+        int x_parent = find(x,parent);
+        int y_parent = find(y,parent);
+        if(x_parent == y_parent){
+            return;
+        }
+        else{
+            parent[x_parent] = y_parent; 
+        }
+
+    }
+    bool equationsPossible(vector<string>& equations) {
+        vector<int>parent(26);
+        int i = 0;
+        for(int i=0;i<26;i++){
+            parent[i] = i;
+        }
+        // firstly evalute all the equality first
+        for(auto word:equations){
+            if(word[1] == '='){
+                int x_parent = find(word[0]-'a',parent);
+                int y_parent = find(word[3]-'a',parent);
+                if(x_parent != y_parent){
+                    Union(word[0]-'a',word[3]-'a',parent);
+                }
+            }
+        }
+        // now evalute the all enequlity sign
+        for(auto word:equations){
+            if(word[1] == '!'){
+                int x = word[0]-'a';
+                int y = word[3]-'a';
+                if(find(x,parent) == find(y,parent)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
