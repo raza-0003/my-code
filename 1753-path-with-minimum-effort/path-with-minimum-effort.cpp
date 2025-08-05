2bf404afe3e1fe52941902aa7750ee6b68617a6e
Faizan Raza <faizanraza2308@gmail.com>
2025-08-05 20:46:47 +0530

    Time: 52 ms (60.75%) | Memory: 24.6 MB (54.24%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
+    typedef pair<int,pair<int,int>>P;
+    int dx[4] = {-1,0,0,1};
+    int dy[4] = {0,-1,1,0};
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>pq;
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
-        dist[0][0]=0;
+        priority_queue<P,vector<P>,greater<P>>pq;
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dist[0][0] = 0;
         pq.push({0,{0,0}});
-        int delRow[]={-1,0,1,0};
-        int delCol[]={0,1,0,-1};
         while(!pq.empty()){
-            auto it = pq.top();
+            pair<int,int> node = pq.top().second;
+            int diff = pq.top().first;
+            int x = node.first;
+            int y = node.second;
+            if(x == n-1 && y == m-1){
+                return diff;
+            }
             pq.pop();
-            int diff = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
-            if(row == n-1 && col == m-1) return diff;
             for(int i=0;i<4;i++){
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                    int newEffort = max(abs(heights[nrow][ncol]-heights[row][col]),diff);
-                    if(newEffort < dist[nrow][ncol]){
-                        dist[nrow][ncol]=newEffort;
-                        pq.push({dist[nrow][ncol],{nrow,ncol}});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && nx < n && ny >=0 && ny < m){
+                    int absDiff = abs(heights[nx][ny] - heights[x][y]);
+                    int maxDiff = max(diff,absDiff);
+                    if(maxDiff < dist[nx][ny]){
+                        dist[nx][ny] = maxDiff;
+                        pq.push({maxDiff,{nx,ny}});
                     }
+                    
                 }
             }
         }
-        return -1;  
+        return dist[n-1][m-1];
+
+        
     }
 };
