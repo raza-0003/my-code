a5caa8d5b955f148f4f843bff638524490fe90f0
Faizan Raza <faizanraza2308@gmail.com>
2025-09-08 23:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (30.14%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     bool isContainZero(int n){
         while(n){
             int digit = n % 10;
@@ -9,20 +8,15 @@ public:
         }
         return false;
     }
+
     vector<int> getNoZeroIntegers(int n) {
-        // brute force approach
-        vector<int>ans;
+        vector<int> ans;
         if(n == 2) return {1,1};
-        for(int i=1;i<=n;i++){
-            ans.push_back(i);
-        }
-        for(int i=0;i<ans.size();i++){
-            int a = ans[i];
-            for(int j=i+1;j<ans.size();j++){
-                if(ans[j] + a == n && !isContainZero(a) && !isContainZero(ans[j])){
-                    return {a,ans[j]};
-                    break;
-                }
+        for(int i=1;i<n;i++){
+            int a = i;
+            int b = n - i;
+            if(!isContainZero(a) && !isContainZero(b)){
+                return {a,b};
             }
         }
         return {};
